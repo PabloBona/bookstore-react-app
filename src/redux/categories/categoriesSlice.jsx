b075d0101c8a039466edf7045cc72fe8cde1dfa2
PabloBona <pablobonasera@gmail.com>
2023-07-17 19:41:25 -0300

    Add store.jsx bookSlice.jsx categoriesSlice.jsx

---
+++ src/redux/categories/categoriesSlice.jsx
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState: 'Under construction',
+  reducers: {},
+});
+
+export default categoriesSlice.reducer;
