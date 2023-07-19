0b53953cd4631c3907ef471e234f6daa3b1a05d0
PabloBona <pablobonasera@gmail.com>
2023-07-19 01:50:15 -0300

    Create an initialState variable for your booksSlice

--- src/redux/books/booksSlice.jsx
+++ src/redux/books/booksSlice.jsx
@@ -2,14 +2,35 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const booksSlice = createSlice({
   name: 'books',
-  initialState: [],
+  initialState: // Initial state:
+  [
+    {
+      itemId: 1,
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      itemId: 2,
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      itemId: 3,
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
   reducers: {
     addBook: (state, action) => {
       state.push(action.payload);
       // imer library changes the state directly
     },
     removeBook: (state, action) => {
-      state.filter((book) => book.id !== action.payload);
+      const bookId = action.payload;
+      return state.filter((book) => book.itemId !== bookId);
     },
   },
 });
