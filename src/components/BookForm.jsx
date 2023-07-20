1641e813d1d2b50ff4eeea8a1338d96c1c5d0a9b
PabloBona <pablobonasera@gmail.com>
2023-07-20 15:19:42 -0300

    Updating BookForm to add a new book in API

--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -1,38 +1,42 @@
 import React, { useEffect, useState } from 'react';
-
-import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
 import axios from 'axios';
-
 import Button from './Button';
 
-function BookForm({
-  // eslint-disable-next-line react/prop-types
-  url,
-}) {
+function BookForm({ url }) {
   const [book, setBook] = useState({});
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
-
+  const [error, setError] = useState(null);
+  console.log(book);
   useEffect(() => {
     const URL = url;
     axios.get(URL)
-      .then((res) => setBook(res.data));
-  });
+      .then((res) => setBook(res.data))
+      .catch((error) => setError(error.message));
+  }, [url]);
+
   const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
 
-  const dispatch = useDispatch();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (book.author && book.title) {
+  const handleAddBook = () => {
+    if (author && title) {
       const newBook = {
-        itemId: Date.now(),
-        author: capitalizeFirstLetter(book.author),
-        title: capitalizeFirstLetter(book.title),
+        item_id: Date.now(),
+        author: capitalizeFirstLetter(author),
+        title: capitalizeFirstLetter(title),
+        category: 'Fiction',
       };
-      dispatch(setBook(newBook));
-      setAuthor('');
-      setTitle('');
+
+      axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/Fm2ydOLEbPj67CnsICQF/books', newBook)
+        .then((res) => {
+          setBook((prevBooks) => ({
+            ...prevBooks,
+            [res.data.item_id]: [res.data],
+          }));
+          setAuthor('');
+          setTitle('');
+        })
+        .catch((error) => setError(error.message));
     }
   };
 
@@ -40,10 +44,9 @@ function BookForm({
     <section className="container">
       <div className="row">
         <div>
-
           <hr />
           <h2 className="text-secondary p-3 text-uppercase">Add New Book</h2>
-          <form className="col-12-sm" onSubmit={handleSubmit}>
+          <form className="col-12-sm">
             <label className="px-3" htmlFor="title">
               <input
                 className="px-2 py-1 rounded border border-secondary"
@@ -68,14 +71,19 @@ function BookForm({
                 required
               />
             </label>
-            <Button onClick={handleSubmit} type="submit">
+            <Button onClick={handleAddBook} type="button">
               <div className="bg-primary rounded p-1 text-white btn-reset">Add New Book</div>
             </Button>
           </form>
+          {error && <div className="error-message">{error}</div>}
         </div>
       </div>
     </section>
   );
 }
+
+BookForm.propTypes = {
+  url: PropTypes.string.isRequired,
+};
 
 export default BookForm;
